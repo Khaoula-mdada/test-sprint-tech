0ddbe0510cfeef4c69c1e5daf405c78657bc2d76
khaoula mdada <khawlaf234@live.fr>
2023-01-11 23:20:11 +0100

    create rooting and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { CreationComponent } from 'src/pages/creation/creation.component';
 import { HomeComponent } from 'src/pages/home/home.component';
 import { UpdateComponent } from 'src/pages/update/update.component';
 import { AppRoutingModule } from './app-routing.module';
 
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { NavigationButtonsComponent } from 'src/components/navigation-buttons/navigation-buttons.component';
+import { HeaderComponent } from 'src/components/header/header.component';
+import { FooterComponent } from 'src/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UpdateComponent,
     CreationComponent,
-    HomeComponent],
+    HomeComponent,NavigationButtonsComponent,HeaderComponent,FooterComponent],
+   
   imports: [
-    BrowserModule, AppRoutingModule
+    BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
